928c87a1e025dada8b54f4249929919ca816951e
JosuaMnl <josuamnalu@gmail.com>
2023-08-13 17:05:20 +0700

    feat: Add provideRepository and provideApiService to AppModule

--- app/src/main/java/com/yosha10/pokepedia/di/AppModule.kt
+++ app/src/main/java/com/yosha10/pokepedia/di/AppModule.kt
@@ -1,10 +1,31 @@
 package com.yosha10.pokepedia.di
 
+import com.yosha10.pokepedia.data.remote.ApiService
+import com.yosha10.pokepedia.repository.PokePediaRepository
+import com.yosha10.pokepedia.utils.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun providePokePediaRepository(api: ApiService) =
+        PokePediaRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideApiService(): ApiService =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(ApiService::class.java)
 }
